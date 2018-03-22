199b7b1341bbf83a9f72b90177544f9d97617628
rob-DEV <masseymann@outlook.com>
2018-03-22 21:51:05 +0000

    Partial GDT transcribing this tutorial http://www.osdever.net/bkerndev/Docs/gdt.htm to GNU AT&T assembly.

--- src/kernel/kernel_main.cpp
+++ src/kernel/kernel_main.cpp
@@ -7,7 +7,6 @@ void kernel_main(void)
     //First off
     terminal_initialize();
 
-
     linebreak();
     terminal_write("------------------------- Welcome to Kernel OS ver 1.0 -------------------------");
     linebreak();
